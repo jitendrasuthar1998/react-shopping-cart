9ffe7e0a2040ae40a62bd767cd08a47da85bc2a3
jitendradev <jitendra.suthar@growcify.com>
2021-11-21 17:21:48 +0530

    created Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
 export const Header = (props) => {
-  return <div>Header</div>
+  return (
+    <header className='row block center'>
+      <div>
+        <a href='#/'>
+          <h1>Small Shopping Cart</h1>
+        </a>
+      </div>
+
+      <div>
+        <a href='#/cart'>Cart</a> <a href='#/signin'>Signin</a>
+      </div>
+    </header>
+  )
 }
