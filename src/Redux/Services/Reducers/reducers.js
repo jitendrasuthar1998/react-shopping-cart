a7cf77ea7fafdca5b4ff5cc582968eee1f932f05
jitendradev <jitendra.suthar@growcify.com>
2021-11-28 19:47:47 +0530

    created rootReducer in Redux

---
+++ src/Redux/Services/Reducers/reducers.js
@@ -0,0 +1,19 @@
+import { ADD_TO_CART } from '../Constants/constants'
+
+const initialState = {
+  cartData: [],
+}
+
+export const cartItems = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartData: action.data,
+      }
+      break
+
+    default:
+      return state
+  }
+}
