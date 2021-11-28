30a8c97ec40fe5034937bdcb7e411b4e470506d1
jitendradev <jitendra.suthar@growcify.com>
2021-11-28 19:30:05 +0530

    Created Component for Redux

--- src/Redux/IndexApp.js
+++ src/Redux/IndexApp.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import User from './User'
-import '../App.css'
+import HomeContainer from './Containers/HomeContainer'
+import './ReduxApp.css'
+
 const IndexApp = () => {
   return (
     <div className='App'>
-      <h1>IndexApp Component</h1>
-      <User data={{ name: 'Ram', age: 20 }} />
-      <User data={{ name: 'Silky', age: 10 }} />
+      <HomeContainer />
     </div>
   )
 }
